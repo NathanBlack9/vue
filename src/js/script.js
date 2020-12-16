75c32e210a180ccb6c2f71f02d7f2d696a7eb9cf
Suren <suren.khachatryan1999@bk.ru>
2020-12-16 19:51:12 +0300

    60% html

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,10 @@ var accordion = document.getElementsByClassName('contentBx');
   
 for (let i = 0; i < accordion.length; i++) {
   accordion[i].addEventListener('click', ()=>{
-    accordion[i].classList.toggle('active');
+    accordion[i].classList.add('active');
+  });
+  accordion[i].addEventListener('dblclick', ()=>{
+    accordion[i].classList.remove('active');
   });
 }
 
